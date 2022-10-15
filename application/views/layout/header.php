fb7eaa490304baac5cc5e92aa16ff28e285ded5e
adipurnama <imadeadipurnamayasa@gmail.com>
2022-10-15 13:25:59 +0800

    Belum Responsive

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -23,7 +23,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap"
         rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="<?= base_url();?>assets/css/styles.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>assets/css/styles.css" rel="stylesheet" />
 </head>
 
 <body class="bg-black">
