3d92968f23d15af7d272e3bed46b7db2280db141
adipurnama <imadeadipurnamayasa@gmail.com>
2022-11-29 17:00:24 +0800

    Update Cost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://tracklessbank.com/';
+$config['base_url'] = 'http://localhost/tracklessbank.com/';
 date_default_timezone_set('Asia/Singapore');
 
 /*
