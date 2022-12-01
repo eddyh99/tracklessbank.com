c50a0ce96b8d5dc35eaa1698115051ebeb47b368
eddy_h99 <eddy_h99@yahoo.com>
2022-12-01 23:10:10 +0800

    Update Topup System

--- application/controllers/m3rc4n73/Swap.php
+++ application/controllers/m3rc4n73/Swap.php
@@ -56,7 +56,7 @@ class Swap extends CI_Controller
                 "amount"    => $amount
             );
 
-            $result = apitrackless("https://api.tracklessbank.com/v1/admin/swap/swap_summary", json_encode($mdata));
+            $result = apitrackless("https://api.tracklessbank.com/v1/trackless/swap/swaptrackless_summary", json_encode($mdata));
             if (@$result->code != 200) {
                 header("HTTP/1.1 500 Internal Server Error");
                 $error = array(
@@ -143,15 +143,10 @@ class Swap extends CI_Controller
                 "quoteid"   => $quoteid,
             );
 
-            $result = apitrackless("https://api.tracklessbank.com/v1/admin/swap/swapProcess", json_encode($mdata));
+            $result = apitrackless("https://api.tracklessbank.com/v1/trackless/swap/swaptracklessProcess", json_encode($mdata));
             if (@$result->code != 200) {
-                header("HTTP/1.1 500 Internal Server Error");
-                $error = array(
-                    "token"     => $this->security->get_csrf_hash(),
-                    "message"   => $result->message
-                );
-                echo json_encode($error);
-                return;
+                $this->session->set_flashdata("failed", $result->message);
+                redirect('m3rc4n73/swap');
             }
 
             $datatc = array(
--- application/views/admin_template/sidebar.php
+++ application/views/admin_template/sidebar.php
@@ -11,6 +11,18 @@
                             <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                             Member
                         </a>
+                        <a class="nav-link collapsed <?= @$mn_op ?>" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#Operations" aria-expanded="false" aria-controls="Operations">
+                            <div class="sb-nav-link-icon"><i class="fas fa-users-cog"></i></div>
+                            Operations
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="Operations" aria-labelledby="headingTwo"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="<?= base_url() ?>m3rc4n73/Operations/topup">Topup</a>
+                            </nav>
+                        </div>                        
                         <a class="nav-link <?= @$mn_currency ?>" href="<?= base_url() ?>m3rc4n73/currency">
                             <div class="sb-nav-link-icon"><i class="fas fa-money-bill"></i></div>
                             Currency
