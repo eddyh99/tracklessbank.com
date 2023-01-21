55de4f39f6994a5c0de110bcd1e2dc115aa5ac47
Adi Purnama <imadeadipurnamayasa@gmail.com>
2023-01-21 15:47:10 +0800

    Update FormatNumber

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -18,7 +18,7 @@
                         <a href="<?= base_url() ?>m3rc4n73/mwallet?cur=<?= $dt->currency ?>">
                             <div class="box-list fw-bold d-flex flex-row py-4 px-4">
                                 <span class="me-auto"><?= $dt->currency ?></span>
-                                <span><?= $dt->symbol ?> <?= $dt->amount ?></span>
+                                <span><?= $dt->symbol ?> <?= number_format($dt->amount,2) ?></span>
                             </div>
                         </a>
                     </div>
@@ -32,7 +32,7 @@
                         <a href="<?= base_url() ?>m3rc4n73/mwallet?cur=<?= $dt->currency ?>">
                             <div class="box-list fw-bold d-flex flex-row py-4 px-4">
                                 <span class="me-auto"><?= $dt->currency ?></span>
-                                <span><?= $dt->symbol ?> <?= $dt->amount ?></span>
+                                <span><?= $dt->symbol ?> <?= number_format($dt->amount,2) ?></span>
                             </div>
                         </a>
                     </div>
@@ -46,7 +46,7 @@
                         <a href="<?= base_url() ?>m3rc4n73/mwallet?cur=<?= $dt->currency ?>">
                             <div class="box-list fw-bold d-flex flex-row py-4 px-4">
                                 <span class="me-auto"><?= $dt->currency ?></span>
-                                <span><?= $dt->symbol ?> <?= $dt->amount ?></span>
+                                <span><?= $dt->symbol ?> <?= number_format($dt->amount,2) ?></span>
                             </div>
                         </a>
                     </div>
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -2,7 +2,8 @@
     <div class="d-flex flex-column me-auto">
         <h3 class="fw-bold text-blue-freedy mb-4">MASTER WALLET <?= $_SESSION["mwallet"] ?></h3>
         <h6 class="fw-bold text-white">Balance</h6>
-        <h4 class="fw-bold text-white"><?= $_SESSION["symbol"] ?> <?= balanceadmin($_SESSION["currency"]) ?></h4>
+        <h4 class="fw-bold text-white"><?= $_SESSION["symbol"] ?>
+            <?= number_format(balanceadmin($_SESSION["currency"]),2) ?></h4>
     </div>
     <img src="<?= base_url() ?>assets/img/tracklessbank/logo-polos.png" alt="" style="height: 75px;" class="me-5">
 </div>
